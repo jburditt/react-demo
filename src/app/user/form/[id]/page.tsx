2ae83fe2f6267e4ad819fa0e615be3a3db0ec79d
Jebb Burditt <jebb.burditt@gmail.com>
2026-03-21 13:31:34 -0700

    add surveyjs

--- src/app/user/form/[id]/page.tsx
+++ src/app/user/form/[id]/page.tsx
@@ -1,13 +1,54 @@
+import { Model } from "survey-core";
+import { Survey } from 'survey-react-ui';
+import { PlainLight } from 'survey-core/themes';
+
 export default async function Page({ params }: any) {
 
-  const { id } = await params;
-  console.log("id", id);
+const surveyJson = {
+      elements: [{
+        name: "firstName",
+        title: "First Name:",
+        type: "text"
+      }, {
+        name: "lastName",
+        title: "Last Name:",
+        type: "text"
+      }, {
+        name: "username",
+        title: "Username:",
+        type: "text"
+      }, {
+        name: "email",
+        title: "Email:",
+        type: "text"
+      }]
+    };
+
+    let survey = new Model(surveyJson);
+    survey.applyTheme(PlainLight);
+    survey.completeText = "Add User";
+    survey.completedHtml = "User added successfully!";
+    // if (this.id()) {
+    //   this.safe(this.userService.apiUserIdGet({ id: this.id()! })).subscribe((user) => {
+    //     console.log(user);
+    //     survey.data = user;
+    //   });
+    // }
+    // TODO handle edit instead of always adding new user
+    survey.onComplete.add((sender, options) => {
+      console.log("Survey results: " + survey.data);
+      // this.safe(this.userService.apiUserPost({ body: survey.data })).subscribe((user) => {
+      //   console.log("User created", user);
+      // });
+    });
+    //this.surveyModel = survey;
+  //}
 
   return (
     <>
-      <h1>User Form</h1>
-      <p>Not implemented yet</p>
-      <p>TODO -&gt; Finish Angular automated user form and then port code to React</p>
+      <h1>Form</h1>
+      <p>An example of automatically generating a form using SurveyJS from a database model.</p>
+      <Survey model={survey} />
     </>
   );
 }
